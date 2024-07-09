ee1cc9ec61491dbd769b8b38d661d988612cfe99
S0fiaMedina <151967289+S0fiaMedina@users.noreply.github.com>
2024-07-09 08:32:42 -0500

    feat: :tada: first commit

---
+++ src/main/java/com/theheroes/theheroes/TheheroesApplication.java
@@ -0,0 +1,13 @@
+package com.theheroes.theheroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheheroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheheroesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theheroes/theheroes/TheheroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theheroes.theheroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheheroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
